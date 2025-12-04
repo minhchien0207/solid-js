d38842acd9cebd4f402a303d1a109e0e462eac96
Chien Bui <minhchien0207@gmail.com>
2025-12-04 17:25:29 +0700

    feat: Add full benefit display page and component.

--- src/components/benefit/BenefitFull.tsx
+++ src/components/benefit/BenefitFull.tsx
@@ -1,7 +1,67 @@
-export default function BenefitFull({ data }: { data: any }) {
+import { splitProps } from 'solid-js';
+// import { createStore } from 'solid-js/store';
+
+type Plan = {
+  [planCode: string]: {
+    id: string;
+    siInWords: string;
+    [key: string]: any;
+  };
+};
+
+type BenefitDetail = {
+  id: string;
+  code: string;
+  name: string;
+  description: string;
+};
+
+type BenefitItem = {
+  benefit: BenefitDetail;
+  index: number;
+  plan: Plan; // Đây chính là object động theo plan_code
+  benefits: BenefitItem[]; // đệ quy nếu có benefit con (trong data của bạn đang là mảng rỗng)
+};
+
+type BenefitFullProps = {
+  data: {
+    plans: string[];
+    benefits: BenefitItem;
+  };
+};
+
+export default function BenefitFull({ data }: BenefitFullProps) {
+  const [local, rest] = splitProps(data, ['plans', 'benefits']);
+  // const [store, setStore] = createStore<{
+  //   plans: string[];
+  //   benefits: BenefitItem[];
+  // }>({
+  //   plans: local.plans,
+  //   benefits: local.benefits,
+  // });
+
   return (
     <div class="flex w-full">
-      <pre>{JSON.stringify(data)}</pre>
+      <div class="tabs tabs-border">
+        {local?.benefits?.benefits?.map((bnfGroup, bnfGroupIndex) => (
+          <>
+            <input
+              type="radio"
+              name="full_benefit"
+              class="tab"
+              aria-label={`${bnfGroup.benefit.name}`}
+            />
+            <div class="tab-content border-base-300 bg-base-100 p-10">
+              <pre class="text-wrap">
+                {JSON.stringify(bnfGroup.benefits, null, 2)}
+              </pre>
+              {/* {bnfGroup.benefit.name} */}
+            </div>
+          </>
+        ))}
+      </div>
+      {/* debug */}
+      {/* <pre class="text-wrap">{JSON.stringify(local, null, 2)}</pre> */}
     </div>
   );
 }
--- src/routes/full_benefit.tsx
+++ src/routes/full_benefit.tsx
@@ -1,11 +1,25 @@
 import { Title } from '@solidjs/meta';
 import BenefitFull from '~/components/benefit/BenefitFull';
+import { createResource, Suspense } from 'solid-js';
 
 export default function FullBenefitPage() {
+  const [benefits] = createResource(() =>
+    fetch('http://localhost:3000/data/full-benefit.json').then((res) =>
+      res.json(),
+    ),
+  );
+
   return (
     <div>
       <Title>Full Benefit</Title>
-      <BenefitFull data={'Loading…'} />
+      <Suspense fallback="Loading...">
+        <BenefitFull
+          data={{
+            plans: ['easy-1', 'easy-2', 'easy-3', 'easy-visa'],
+            benefits: benefits() ?? [],
+          }}
+        />
+      </Suspense>
     </div>
   );
 }
